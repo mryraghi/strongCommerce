6ee5e241040337adc289b14e6c3981350ecdf0d4
Romeo <romeo.bellon@me.com>
2017-05-21 10:55:41 +0200

    Dokku setup

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = '3000'; //process.env.PORT ||
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 /**
